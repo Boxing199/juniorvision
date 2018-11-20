59428521cbbe1965b81545b82865661baac81e6e
Virko12345 <virkolomeo@gmail.com>
2018-11-20 12:47:27 +0200

    adedd mobile menu

--- dist/js/mobile-menu.js
+++ dist/js/mobile-menu.js
@@ -1,18 +1,7 @@
-/*
-var toggleBox = document.getElementsByClassName('.mobile-menu');
 
-document.getElementsByClassName('.menu-toggle').click = function() {
-  if (toggleBox.ClassName === ".open") {
-    toggleBox.classList.remove(".open");
-  }
-  else {
-    toggleBox.classList.add(".open");
-  }
-};
-*/
-$(document).ready(function() {
-  $(".menu-toggle").click(function() {	
-  $(".mobile-menu").toggle(0, 1);	
-  });	
-  	
+var toggleBox = document.getElementById('mobile-menu'),
+    toggleMenu = document.getElementById('menu-toggle');
+
+  toggleMenu.addEventListener("click", function() {
+    toggleBox.classList.toggle("open");
 });
